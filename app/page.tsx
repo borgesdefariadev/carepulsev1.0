1a3fc1266d9b107116bb8ffba8ef6219df96d168
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2025-10-08 13:50:36 -0300

    Update
    commit 3

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,9 @@ export default function Home() {
 
   return (
     <div>
-      <h1 className="text-3xl underline">Home</h1>
+      <h1 className="text-white not-first:text-3xl underline">Home</h1>
       <Button>Click me</Button>
+      
     </div>
   );
 }
