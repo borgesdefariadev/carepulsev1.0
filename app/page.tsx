a30db2657b624c9b32e5252c732bb49c025c84b1
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2025-10-09 15:23:55 -0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,27 @@
 import Image from "next/image";
+import PatientForm from "@/components/forms/PatientForm";
 
 export default function Home() {
-
   return (
     <div className="flex h-screen max-h-screen">
-    <section className="remove-scrollbar container my-auto ">
-      <div className="sub-container max-w-[496px]">
-      <Image
-        src="/assets/icons/logo-full.svg"
-        alt="patient"
-        width={1000}
-        height={1000}
-        className="mb-12 h-10 w-fit"
-      />
+      <section className="remove-scrollbar container my-auto ">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt="patient"
+            width={1000}
+            height={1000}
+            className="mb-12 h-10 w-fit"
+          />
 
-      </div>
-    </section>
-      
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              2025 © All Rights Reserved. Made with ❤️ by{"3eezy"}
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
