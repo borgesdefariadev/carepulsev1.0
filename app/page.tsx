d7eda5e9f358552a5572dfad1741fb0cb710d650
Ricardo Borges de Faria Filho <borgesdefariadev@gmail.com>
2025-10-10 12:16:42 -0300

    Update
    Started the idea of forms, input and form validation, also made some more configs and updates

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
       {/* TODO : OTP VERIFICATION | Passkey Modal */}
-      
+
       <section className="remove-scrollbar container my-auto ">
         <div className="sub-container max-w-[496px]">
           <Image
